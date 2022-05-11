4c5c469080ac932ab4d006616f3a1f0c2de77fea
Carlos-Who <carlosdiaz.devx@gmail.com>
2022-05-11 01:25:11 -0500

    [Update] The routes controllers have been changed to laravel 8 syntax

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products','\App\Http\Controllers\ProductController@index');
-Route::post('/products','\App\Http\Controllers\ProductController@store');
-Route::put('/products/{id}','\App\Http\Controllers\ProductController@update');
-Route::delete('/products/{id}','\App\Http\Controllers\ProductController@destroy');
+
+Route::get('/products', [ProductController::class,'index']);
+Route::post('/products', [ProductController::class,'store']);
+Route::put('/products/{id}', [ProductController::class,'update']);
+Route::delete('/products/{id}', [ProductController::class,'destroy']);
